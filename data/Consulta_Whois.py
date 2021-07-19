6fe9596972e9c1ebb35996a62a8fa09f7ef11f41
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-19 01:17:00 -0300

    Update Consulta_Whois.py

--- data/Consulta_Whois.py
+++ data/Consulta_Whois.py
@@ -1,14 +1,20 @@
-import os
-import sys
+import socket
+global R, B, C, Y, G, RT, CY, CO
+CO='\033[m'
+R='\033[1;31m'
+B='\033[1;34m'
+C='\033[1;37m'
+CY='\033[1;36m'
+Y='\033[1;33m'
+G='\033[1;32m'
+RT='\033[;0m'
 def main():
+			alvo = input(f"{C}[{Y}!{C}] {G}Ip do Alvo ~/ ")
 			try:
-				import whois
+				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				sock.connect_ex((alvo, 22))
+				print(sock.recv(2048))
+				sock.close()
 			except:
-				os.system("pip install whois")
-				import whois
-			dominio = input("IP ALVO: ")
-			consulta_whois = whois.whois(dominio)
-			try:
-				print(consulta_whois.text);time.sleep(5)
-			except:
-				pass
+			  import time
+			  print(f"{R}Nenhuma info associada ao Host!");time.sleep(3);pass
