498efe5665f96cfdeced92b78d940cb10bb409ac
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-18 21:37:12 -0300

    Update Banner_Grabbing.py

--- data/Banner_Grabbing.py
+++ data/Banner_Grabbing.py
@@ -1,7 +1,11 @@
 import socket
 def main():
 			alvo = input("ALVO: ")
-			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock.connect_ex((alvo, 22))
-			print(sock.recv(2048))
-			sock.close()
+			try:
+				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				sock.connect_ex((alvo, 22))
+				print(sock.recv(2048))
+				sock.close()
+			except:
+			  import time
+			  print("Nenhuma info associada ao Host");time.sleep(3);pass
