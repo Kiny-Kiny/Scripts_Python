a3d186908e023513c1a1dd65d069fb95571ed701
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-19 01:18:32 -0300

    Update Banner_Grabbing.py

--- data/Banner_Grabbing.py
+++ data/Banner_Grabbing.py
@@ -1,6 +1,15 @@
 import socket
+global R, B, C, Y, G, RT, CY, CO
+CO='\033[m'
+R='\033[1;31m'
+B='\033[1;34m'
+C='\033[1;37m'
+CY='\033[1;36m'
+Y='\033[1;33m'
+G='\033[1;32m'
+RT='\033[;0m'
 def main():
-			alvo = input("ALVO: ")
+			alvo = input(f"{C}[{Y}!{C}] {G}Ip do Alvo ~/ ")
 			try:
 				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 				sock.connect_ex((alvo, 22))
@@ -8,4 +17,4 @@ def main():
 				sock.close()
 			except:
 			  import time
-			  print("Nenhuma info associada ao Host");time.sleep(3);pass
+			  print(f"{R}Nenhuma info associada ao Host!");time.sleep(3);pass
